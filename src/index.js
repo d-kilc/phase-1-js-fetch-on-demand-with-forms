7686779f5ff0707f91e87311e3d0b4a1581e8f09
Daniel <dkilcullen25@gmail.com>
2021-12-21 17:04:40 -0600

    commit tree 5c927c4c80a7ae5ce9a983e7c4553069a55d01b4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        fetch(`http://localhost:3000/movies/${e.target[0].value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const title = document.querySelector('#movieDetails h4')
+            const summary = document.querySelector('#movieDetails p')
+            
+            title.textContent = data.title
+            summary.textContent = data.summary
+        })
+
+        e.target.reset()
+    })
 }
 
-document.addEventListener('DOMContentLoaded', init);
+init()
